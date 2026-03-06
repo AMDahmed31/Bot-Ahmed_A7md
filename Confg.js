83f31c1ba771597949fcfb7b283a2730ce2aca82
AMDahmed31 <ahmed20096ad@gmail.com>
2026-03-07 01:18:40 +0200

    add all

--- Confg.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    ANTHROPIC_API_KEY: 'حط الـ Anthropic Key هنا',
-    SOUNDCLOUD_CLIENT_ID: '1cRPaCl9errnBF4yZebfxmliKprCemRI'
-};
--- commands/auto_reaction.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    commands: [], 
-    async execute(sock, msg, from, text) {
-    }
-}
